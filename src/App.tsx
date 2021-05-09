2ea18d59e8f6b34627545ec71d63def95621a2ec
harsh-kumar-saaniya <harshvithal1@gmail.com>
2021-05-09 12:12:10 -0700

    Added Line in font of background image

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
       <BackgroundConfig />
 
 
-      {/* <Launch /> */}
+      <Launch />
       {/* <LaunchesUi /> */}
       {/* <LaunchDetails />  */}
     </div>
--- src/components/BackgroundStuff/BackgroundConfig.tsx
+++ src/components/BackgroundStuff/BackgroundConfig.tsx
@@ -5,8 +5,9 @@ const BackgroundConfig = () => {
     return (
         <div className="background-container">
             <div className="backgroundImage">
-                hello world
-            {/* <img src="https://investorplace.com/wp-content/uploads/2021/03/astronaut.jpg"/> */}
+                <div className="heading">
+                    <p>Bootcamp Project 8 SpaceX App</p>
+                </div>
             </div>
         </div>
     )
