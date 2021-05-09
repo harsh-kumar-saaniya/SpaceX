68e869c0abc8f4d5ddc93a3dd7c7e17a230a1ed5
harsh-kumar-saaniya <harshvithal1@gmail.com>
2021-05-09 01:19:03 -0700

    Fixed Issue in master there was merging issue when fontComp component has merged

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,20 @@ import './App.css';
 import Launch from './components/Launch';
 import LaunchDetails from './components/LaunchDetails';
 import Header from './components/Header/Header';
-<<<<<<< HEAD
-=======
 import FontComp from './components/FontComp/FontComp';
 import LaunchesUi from './components/LaunchesUI/Launches.ui';
->>>>>>> FontComp
 
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-<<<<<<< HEAD
       {/* <Launch />
       <LaunchDetails />  */}
-=======
       <FontComp />
       {/* <Launch /> */}
       {/* <LaunchesUi /> */}
       {/* <LaunchDetails />  */}
->>>>>>> FontComp
     </div>
   );
 }
--- src/components/FontComp/FontComp.js
+++ src/components/FontComp/FontComp.js
@@ -1,21 +1,6 @@
-// import React from 'react';
-// import '../Header/Header.css';
+import React from 'react';
+import '../Header/Header.css';
 
-<<<<<<< HEAD:src/components/BackgroundComponent/Background.js
-// const Background = () => {
-//     return (
-//         <div>
-//             <div className="background-image-container">
-//                 <img src="https://investorplace.com/wp-content/uploads/2021/03/astronaut.jpg" />
-//             </div>
-//             <div className="launches">
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Background;
-=======
 const FontComp = () => {
     return (
         <div>
@@ -29,7 +14,5 @@ const FontComp = () => {
 }
 
 export default FontComp;
->>>>>>> FontComp:src/components/FontComp/FontComp.js
-
 
 ///// NEEED TO CHANGE THE NAME OF THIS FILE AND THIS FOLDER NEED TO MAKE IT MEANINGFULL IT'S NOT MAKING SENSE.
