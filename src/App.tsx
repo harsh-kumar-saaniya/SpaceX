97da8158966c53fecf18a6eefb3ea0f79f490c0f
harsh-kumar-saaniya <harshvithal1@gmail.com>
2021-05-09 23:40:52 -0700

    Added LaunchesUI data

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,22 @@ import LaunchDetails from './components/LaunchDetails';
 import Header from './components/Header/Header';
 import LaunchesUi from './components/LaunchesUI/Launches.ui';
 import BackgroundConfig from './components/BackgroundStuff/BackgroundConfig';
+import LaunchesList from './components/LauchesUI/LaunchesList';
+
+
+
+
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <BackgroundConfig />
+      <LaunchesList />
       {/* <Launch />
       <LaunchDetails />  */}
       {/* <Launch /> */}
-      <Launch />
+      {/* <Launch /> */}
       {/* <LaunchesUi /> */}
       {/* <LaunchDetails />  */}
     </div>
---
+++ src/components/LauchesUI/LaunchesList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './LauchesList.css';
+
+const LaunchesList = () => {
+    return (
+        <div className="launchesList">
+            <div className="launchesContainer">
+                <div className="listItems">
+                    <div className="missionName"><h2>Mission Name: Falcon</h2></div>
+                    <div className="missionYear"><p>Mission Year: 2009 - Flight: success</p></div>
+                    <div className="Detail"><button>DETAIL </button></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LaunchesList;
